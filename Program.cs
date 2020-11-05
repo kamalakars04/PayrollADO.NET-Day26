23c1768c2205efde9457c08c64493714d700d2dc
kamalakars04 <kamalakars04@gmail.com>
2020-11-05 21:47:20 +0530

    [Kamalakar].[Refactor].Adding UC7_AddEmployee

--- Program.cs
+++ Program.cs
@@ -25,7 +25,24 @@ namespace EmployeePayroll
             // UC 5 Retrieve with start date
             payrollService.RetrieveWithStartDate(new DateTime(2014, 01, 01), new DateTime(2019, 01, 01));
 
+            // UC 6 find min max sum avg of salary
             payrollService.PerformSumAvgMinMax();
+
+            // UC 7 Add employee with ER
+            // Create employee class
+            EmployeeDetails employee = EmployeeDetails.GetInstance();
+            employee.empName = "Ravan";
+            employee.gender = 'M';
+            employee.phoneNumber = "9874569874";
+            employee.startDate = new DateTime(2019, 01, 05);
+            employee.payrollId = 2;
+
+            // Create address instance
+            EmpAddress address = EmpAddress.GetInstance();
+            address.street = "gandhi nagar";
+            address.city = "pune";
+            address.state = "Maharastra";
+            payrollService.AddEmployee(employee, address);
         }
     }
 }
